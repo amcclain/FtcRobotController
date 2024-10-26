8fbadd60130f5763c600f06eea52405aabb2fd10
Anselm McClain <atm@xh.io>
2024-10-26 06:30:07 -0700

    Checkpoint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CrabBotDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CrabBotDemo.java
@@ -1,12 +1,12 @@
-package org.thewaverlyschool.robotics;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "CrabBotAS")
-public class CrabBotAS extends LinearOpMode {
+@TeleOp(name = "CrabBot Demo 2023-2024")
+public class CrabBotDemo extends LinearOpMode {
 
     @Override
     public void runOpMode() {
